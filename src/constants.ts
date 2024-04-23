6c3deca14674a902c47cbe7a7c24b018ada5e9b4
river <laolei@forkway.cn>
2024-04-23 19:13:55 +0800

    init repo

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const GPTsMotion_ELEMENT_SELECTOR = 'data-GPTsMotion-node-id';
+
+// read from env
+export const debugMode = process.env.DEBUG_MODE === 'true';
---
+++ src/pages/Devtools/index.js
@@ -0,0 +1,1 @@
+chrome.devtools.panels.create('GPTsMotion AI', 'icon-34.png', 'panel.html');
